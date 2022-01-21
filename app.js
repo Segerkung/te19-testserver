b8ca4ea3fe31983ff98932be25e666ad8338f75c
*Segerkung* <*leo.segerljung@elev.ga.ntig.se*>
2022-01-21 14:55:25 +0100

    asd

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const sassMiddleware = require('node-sass-middleware');
-
+require('dotenv').config();
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const tasksRouter = require('./routes/tasks');
+
+
 
 const app = express();
 
@@ -23,5 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/tasks', tasksRouter);
+
+
 
 module.exports = app;
