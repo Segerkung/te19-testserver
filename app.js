7f62c52a4d85038cd013908179b1105c831cd04c
Segerkung <60388987+Segerkung@users.noreply.github.com>
2022-02-11 09:06:11 +0100

    jkh,

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const sassMiddleware = require('node-sass-middleware');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const tasksRouter = require('./routes/tasks');
+
 
 const app = express();
 
@@ -23,5 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/tasks', tasksRouter);
+
 
 module.exports = app;
