7f2c133806299dc542c0f342c3d486abb942221f
Segerkung <60388987+Segerkung@users.noreply.github.com>
2022-02-18 08:49:06 +0100

    asd

--- routes/tasks.js
+++ routes/tasks.js
@@ -2,13 +2,47 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../database');
 /* 
-    BASE URL /tasks
-    GET / - Get all tasks
-    POST / - Create a new task
+BASE URL /tasks
+GET / - Get all tasks
+POST / - Create a new task
     GET /:id - Get a task by id
     PUT /:id - Update a task by id
     DELETE /:id - Delete a task by id
 */
+
+router.get('/:id', async (req, res, next) => {
+    const id = req.params.id;
+    
+    if (isNaN(req.params.id)) {
+        res.status(400).json({
+            task: {
+                error: 'Bad request'
+            }
+        });
+    }
+    await pool.promise()
+    .query('SELECT * FROM tasks WHERE id = ?', [id])
+        .then(([rows, fields]) => {
+            res.json({
+                tasks: {
+                    data: rows
+                }
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                tasks: {
+                    error: 'Error getting tasks'
+                }
+            })
+        });
+        // res.json({
+    //     id: req.params.id
+    // })
+    
+});
+
 router.get('/', async (req, res, next) => {
     await pool.promise()
         .query('SELECT * FROM tasks')
@@ -28,48 +62,14 @@ router.get('/', async (req, res, next) => {
             })
         });
 });
-
-router.get('/:id', async (req, res, next) => {
-    const id = req.params.id;
-
-    if (isNaN(req.params.id)) {
-        res.status(400).json({
-            task: {
-                error: 'Bad request'
-            }
-        });
-    }
-    await pool.promise()
-        .query('SELECT * FROM tasks WHERE id = ?', [id])
-        .then(([rows, fields]) => {
-            res.json({
-                tasks: {
-                    data: rows
-                }
-            });
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json({
-                tasks: {
-                    error: 'Error getting tasks'
-                }
-            })
-        });
-    // res.json({
-    //     id: req.params.id
-    // })
-
-});
-
 module.exports = router;
 
 
 
 /*
-    await pool
-    .promise()
-    .query('SELECT * FROM users')
+await pool
+.promise()
+.query('SELECT * FROM users')
     .then(([rows, fields]) => {
         res.json({
             data: rows,
